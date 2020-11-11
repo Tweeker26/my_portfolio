dbc74a72dea772915a76bea9c594cd38d3a3ea13
tweeker26 <ivanwin26@gmail.com>
2020-11-11 22:13:18 +0300

    some fixes and upgrades 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
+        icon: `src/assets/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     {
